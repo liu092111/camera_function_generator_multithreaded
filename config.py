3ebd1f30e889124aa7e84693d10ef9cb263257b4
liu092111 <liu092111@gmail.com>
2026-02-01 22:16:25 +0800

    add 20260201 experiment data

--- config.py
+++ config.py
@@ -7,7 +7,7 @@
 import numpy as np
 
 # ========== 攝影機設定 ==========
-MODE = "straight"  # "straight" 或 "rotation"
+MODE = "rotation"  # "straight" 或 "rotation"
 CAMERA_INDEX = 1
 CAM_WIDTH = 640    # 攝影機解析度寬度
 CAM_HEIGHT = 480   # 攝影機解析度高度
@@ -27,7 +27,7 @@ FLIP_VERTICAL = True     # 垂直翻轉（上下翻轉），修正 device 移動
 FLIP_HORIZONTAL = True  # 水平翻轉（左右翻轉）
 
 # 統一電壓設定（V)
-VOLTAGE = 1.4
+VOLTAGE = 1.2
 
 # ========== PID 控制設定 ==========
 ENABLE_PID_CONTROL = False  # 是否啟用 PID 控制
@@ -96,7 +96,7 @@ ORIENT_YLIM_DEG = 60            # 角度圖 Y 軸範圍（±度）
 PLOT_RANGE_SCALE = 1.35         # 圖表範圍縮放因子
 
 # ========== 函數產生器設定 ==========
-FG_RESOURCE_STRING = 'USB0::0x0957::0x5707::MY59001615::0::INSTR'
+FG_RESOURCE_STRING = 'USB0::0x0957::0x5707::MY59000656::0::INSTR'
 
 # 模式配置
 FG_MODE_CONFIGS = {
--- dual_modal_selector_4modes.py
+++ dual_modal_selector_4modes.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     print("Connecting...")
     
     rm = visa.ResourceManager()
-    inst = rm.open_resource('USB0::0x0957::0x5707::MY59001615::0::INSTR')
+    inst = rm.open_resource('USB0::0x0957::0x5707::MY59000656::0::INSTR')
     
     try:
         inst.control_ren(6)
